dddac140cd9e961a6517a69bc8250ef4a46dba75
Dieter Baron <dillo@nih.at>
2024-05-20 15:21:55 +0200

    Add startup screen.

--- src/start.s
+++ src/start.s
@@ -4,14 +4,49 @@ color_ram = $d800
 .section code
 
 .public start {
-    jsr setup_display
-
+    jsr startup_display
     jsr clocks_init
+    jsr setup_display
     jsr clocks_init_display
 
 loop:
     jsr clocks_update
     jmp loop
+}
+
+startup_display {
+    lda #VIC_VIDEO_ADDRESS($400, $2000)
+    sta VIC_VIDEO_ADDRESS
+    lda #FRAME_COLOR
+    sta VIC_BORDER_COLOR
+    sta VIC_BACKGROUND_COLOR
+    ldx #0
+    lda #$20
+:   sta screen,x
+    sta screen + $100,x
+    sta screen + $200,x
+    sta screen + $2e8,x
+    dex
+    bne :-
+    lda #PARAMETER_COLOR
+:   sta color_ram,x
+    sta color_ram + $100,x
+    sta color_ram + $200,x
+    sta color_ram + $2e8,x
+    dex
+    bne :-
+:   lda startup_message,x
+    beq done
+    sta screen + 10 * 40 + 12,x
+    inx
+    bne :-
+done:
+    ldx #5
+:   lda main_screen + 1000 - 5,x
+    and #$7f
+    sta screen + 1000 - 5,x
+    dex
+    bpl :-
 }
 
 setup_display {
@@ -50,6 +85,12 @@ setup_display {
 }
 
 .pin charset $2000
+
+.section data
+
+startup_message {
+    .data "detecting clocks":screen, 0
+}
 
 .section zero_page
 
